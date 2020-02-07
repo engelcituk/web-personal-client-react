4db5e101bdab840d1516c8b9c9e5a2805ac5be85
cituk <programaciondyn3.mx@sandos.com>
2020-02-06 23:23:53 -0500

    Creando la configuración de rutas para el Panel de Administrador

---
+++ src/config/routes.js
@@ -0,0 +1,28 @@
+//layout
+import LayoutAdmin from '../layouts/LayoutAdmin'
+
+//admin  pages
+import AdminHome from '../pages/Admin/';
+import AdminSigIn from '../pages/Admin/SigIn';
+
+const routes = [
+    {
+        path: "/admin",
+        component: LayoutAdmin,
+        exact: false,
+        routes: [
+            {
+                path:"/admin",
+                component: AdminHome,
+                exact: true
+            },
+            {
+                path:"/admin/login",
+                component: AdminSigIn,
+                exact: true
+            }
+        ]
+    }
+];
+
+export default routes;git s
