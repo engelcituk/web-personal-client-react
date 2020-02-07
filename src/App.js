2e515fd8636267df1c795f71ae67af532cc2507f
cituk <programaciondyn3.mx@sandos.com>
2020-02-06 22:33:54 -0500

    Ejemplo de uso React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './App.scss';
-import {} from "module";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
-    return (
-    <div className="App">
-        <h1> Web Personal</h1>
-    </div>
+    return ( 
+        <Router>
+            <div className = "App" >
+                <h1 > Sistema de rutas basico </h1>
+                <Link to="/">Home</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/users">Users</Link>
+
+                <Switch>
+                    <Route exact path="/"  component={Home}/>
+                    <Route exact path="/contact"  component={Contact}/>
+                    <Route exact path="/users"  component={Users}/>
+                    <Route component={Error404}/>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
+function Home() {
+    return <h1> Estamos en el componente home </h1>;
+}
+
+function Contact() {
+    return <h1> Estamos en el componente contac </h1>;
+}
+
+function Users() {
+    return <h1> Estamos en el componente userss </h1>;
+}
+function Error404() {
+    return <h1> Estamos en el componente not found </h1>;
+}
 export default App;
