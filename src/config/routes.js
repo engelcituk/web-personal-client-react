036a26e3b42de7d41f0e1dcad31fd7c046ef3193
cituk <programaciondyn3.mx@sandos.com>
2020-02-07 22:31:51 -0500

    Añadiendo la configuración de rutas para usuarios normales no logueado

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,28 +1,52 @@
 //layout
 import LayoutAdmin from '../layouts/LayoutAdmin'
+import LayoutBasic from '../layouts/LayoutBasic'
 
 //admin  pages
 import AdminHome from '../pages/Admin/';
 import AdminSigIn from '../pages/Admin/SigIn';
 
+//pages
+import Home from '../pages/Home';
+import Contact from '../pages/Contact';
+
+
 const routes = [
     {
-        path: "/admin",
-        component: LayoutAdmin,
-        exact: false,
-        routes: [
-            {
-                path:"/admin",
-                component: AdminHome,
-                exact: true
-            },
-            {
-                path:"/admin/login",
-                component: AdminSigIn,
-                exact: true
-            }
-        ]
-    }
+    path: "/admin",
+    exact: false,
+    component: LayoutAdmin,
+    routes: [{
+            path: "/admin",
+            exact: true,
+            component: AdminHome
+        },
+        {
+            path: "/admin/login",
+            exact: true,
+            component: AdminSigIn
+        }
+    ]
+},
+{
+    path: "/",
+    component: LayoutBasic,
+    exact: false, //#endregion
+    routes: [
+        {
+            path: "/",
+            component: Home,
+            exact: true,
+
+        },
+        {
+            path: "/contact",
+            component: Contact,
+            exact: true,
+
+        }
+    ]
+}
 ];
 
 export default routes;
