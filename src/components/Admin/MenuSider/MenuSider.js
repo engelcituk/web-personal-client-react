6c57d7a879cd15a9eeb97f8b1e15401f09377de0
cituk <programaciondyn3.mx@sandos.com>
2020-02-10 23:30:06 -0500

    Solucionando cambio del menú al recargar la página

--- src/components/Admin/MenuSider/MenuSider.js
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, withRouter} from 'react-router-dom';
 import { Layout, Menu, Icon } from 'antd';
 
 import "./MenuSider.scss";
 
-export default function MenuSider(props) {
-    const {menuCollapsed}= props;
+function MenuSider(props) {
+    const {menuCollapsed, location}= props;
     const {Sider} = Layout;
 
     return (
         <Sider className="admin-sider" collapsed={menuCollapsed}>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                <Menu.Item key="1">
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname]}>
+                <Menu.Item key="/admin">
                     <Link to={"/admin"}>
                         <Icon type="home"/>
                         <span className="nav-text">Home</span>
                     </Link>
                 </Menu.Item>
-                <Menu.Item key="2">
+                <Menu.Item key="/admin/users">
                     <Link to={"/admin/users"}>
                         <Icon type="user"/>
                         <span className="nav-text">Usuarios</span>
@@ -27,3 +27,4 @@ export default function MenuSider(props) {
         </Sider>
     );
 }
+export default withRouter(MenuSider);
