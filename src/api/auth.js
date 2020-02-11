480408f3b2b69706cba121aa8ef8228063ed2771
cituk <programaciondyn3.mx@sandos.com>
2020-02-10 20:38:16 -0500

    Creando función para refrescar el AccessToken desde el cliente

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import { basePath, apiVersion } from './config';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../utils/constants';
 import jwtDecode from 'jwt-decode';
 
-export function getAccessToken() {
+export function getAccessTokenApi() {
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
     if (!accessToken || accessToken === "null") {
         return null;
@@ -10,7 +10,7 @@ export function getAccessToken() {
     return willExpireToken(accessToken) ? null : accessToken;
 
 }
-export function getRefreshToken() {
+export function getRefreshTokenApi() {
     const refresToken = localStorage.getItem(REFRESH_TOKEN);
 
     if (!refresToken || refresToken === "null") {
@@ -19,11 +19,44 @@ export function getRefreshToken() {
     return willExpireToken(refresToken) ? null : refresToken;
 }
 
+export function refreshAccessToken(refreshToken) {
+    const url = `${basePath}/${apiVersion}/refresh-access-token`;
+    const bodyObj = {
+        refresToken: refreshToken
+    }
+    const params = {
+        method: "POST",
+        body: JSON.stringify(bodyObj),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    return fetch(url, params)
+        .then(response => {
+            if (response.status !== 200) {
+                return null;
+            }
+            return response.json();
+        })
+        .then(result => {
+            if (!result) {
+                //desloguear al usuario
+            } else {
+                const { accessToken, refreshToken } = result;
+                localStorage.setItem(ACCESS_TOKEN, accessToken);
+                localStorage.setItem(REFRESH_TOKEN, refreshToken);
+            }
+        })
+        .catch(err => {
+            return err.message;
+        })
+}
+
 function willExpireToken(token) {
     const seconds = 60;
     const metaToken = jwtDecode(token);
     const { exp } = metaToken;
     const now = (Date.now() + seconds) / 1000;
     return now > exp;
-
 }
