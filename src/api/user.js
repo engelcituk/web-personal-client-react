60598aee122c6cca62934ea43e7f021d9d760264
cituk <programaciondyn3.mx@sandos.com>
2020-02-16 22:00:36 -0500

    Creando funciones Subir Avatar, Obtener Avatar y Actualizar Usuario en cliente

--- src/api/user.js
+++ src/api/user.js
@@ -62,7 +62,7 @@ export function signInApi(data) {
         })
         .then(result => {
             
-            return result;
+            return result; 
         })
         .catch(err => {
             return err.message ;
@@ -88,5 +88,69 @@ export function signInApi(data) {
         })
         .catch(err => {
             return err.message ;
+        })
+ }
+
+ export function uploadAvatarApi(token, avatar, userId){
+    const url = `${basePath}/${apiVersion}/upload-avatar/${userId }`;
+
+    const formData = new FormData();
+    formData.append("avatar", avatar, avatar.name);
+
+    const params = {
+        method: "PUT",
+        body: formData,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization":token
+        }
+    }
+
+    return fetch(url, params)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            return result; 
+        })
+        .catch(err => {
+            return err.message;
+        })
+ }
+
+ export function getAvatarApi(avatarName){
+    const url = `${basePath}/${apiVersion}/get-avatar/${avatarName }`;
+
+    return fetch(url)
+        .then(response => {
+            return response.url;
+        })
+        .catch(err => {
+            return err.message;
+        })
+ }
+
+ export function updateUserApi(token, user, userId){
+    const url = `${basePath}/${apiVersion}/users/${userId }`;
+
+
+    const params = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization":token 
+        },
+        body: JSON.stringify(user),
+    }
+
+    return fetch(url, params)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            return result; 
+        })
+        .catch(err => {
+            return err.message;
         })
  }
