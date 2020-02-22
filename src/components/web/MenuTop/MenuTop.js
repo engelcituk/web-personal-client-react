44fa0b6397f4de2eb5bd69886ecb0a55e3c07466
cituk <programaciondyn3.mx@sandos.com>
2020-02-22 01:15:42 -0500

    Añadiendo el logo de la web al menú

--- src/components/web/MenuTop/MenuTop.js
+++ src/components/web/MenuTop/MenuTop.js
@@ -2,6 +2,7 @@ import React ,{useState, useEffect} from 'react'
 import './MenuTop.scss';
 import { Menu} from 'antd';
 import {Link} from 'react-router-dom';
+import Logo from '../../../assets/img/png/logo-white.png'
 
 
 export default function MenuTop() {
@@ -9,7 +10,9 @@ export default function MenuTop() {
     return (
       <Menu className="menu-top" mode="horizontal">
           <Menu.Item className="menu-top__logo">
-          logo...
+            <Link to={"/"}>
+                <img src={Logo} alt="logo"/> 
+            </Link>
           </Menu.Item>
           <Menu.Item className="menu-top__item">
             <Link to="/">Home</Link>
