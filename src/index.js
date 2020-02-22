354f50cf80741d956c4a2ac596f36464cbbb5122
cituk <programaciondyn3.mx@sandos.com>
2020-02-22 15:33:24 -0500

    Obteniendo el menu de la base de datos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.scss';
-
+import './App.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
