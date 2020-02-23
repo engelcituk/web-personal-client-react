6b65acd4d8f537f8ec85e4a5597500a14b60fd49
cituk <programaciondyn3.mx@sandos.com>
2020-02-23 00:43:20 -0500

    2/2 - Añadiendo sección de reviews de usuarios

--- src/components/web/ReviewCourses/ReviewCourses.js
+++ src/components/web/ReviewCourses/ReviewCourses.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ReviewCourses.scss';
-import { Row, Col, Card, avatar, Avatar} from 'antd';
+import { Row, Col, Card, Avatar} from 'antd';
 import AvatarPersona from '../../../assets/img/png/no-avatar.png'
 
 
@@ -17,11 +17,57 @@ export default function ReviewCourses() {
                 </Row>
                 <Row>
                     <Col lg={4}/>
-                    <Col lg={16} className="reviews-courses__title">
+                    <Col lg={16}    >
                         <Row className="row-cards">
                             <Col md={8}>
-                                <CardReview/>
+                                <CardReview
+                                    name="Alonso Cardenas"
+                                    subtitle="Alumno de Udemy"
+                                    avatar={AvatarPersona}
+                                    review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure corrupti nemo dolore odio saepe ex? Cum nisi dolores, neque, reprehenderit suscipit nemo, debitis voluptas consequuntur harum possimus praesentium iure."
+                                />
                             </Col>
+                            <Col md={8}>
+                                <CardReview
+                                    name="David Peraza"
+                                    subtitle="Alumno de Udemy"
+                                    avatar={AvatarPersona}
+                                    review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure corrupti nemo dolore odio saepe ex? Cum nisi dolores, neque, reprehenderit suscipit nemo, debitis voluptas consequuntur harum possimus praesentium iure."
+                                />
+                            </Col>
+                            <Col md={8}>
+                                <CardReview
+                                    name="Mario Perez"
+                                    subtitle="Alumno de Udemy"
+                                    avatar={AvatarPersona}
+                                    review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure corrupti nemo dolore odio saepe ex? Cum nisi dolores, neque, reprehenderit suscipit nemo, debitis voluptas consequuntur harum possimus praesentium iure."
+                                />
+                            </Col>
+                            <Col md={8}>
+                                <CardReview
+                                    name="Jorge Rodriguez"
+                                    subtitle="Alumno de Udemy"
+                                    avatar={AvatarPersona}
+                                    review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure corrupti nemo dolore odio saepe ex? Cum nisi dolores, neque, reprehenderit suscipit nemo, debitis voluptas consequuntur harum possimus praesentium iure."
+                                />
+                            </Col>
+                            <Col md={8}>
+                                <CardReview
+                                    name="Juan Ramos"
+                                    subtitle="Alumno de Udemy"
+                                    avatar={AvatarPersona}
+                                    review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure corrupti nemo dolore odio saepe ex? Cum nisi dolores, neque, reprehenderit suscipit nemo, debitis voluptas consequuntur harum possimus praesentium iure."
+                                />
+                            </Col>
+                            <Col md={8}>
+                                <CardReview
+                                    name="José Vázquez"
+                                    subtitle="Alumno de Udemy"
+                                    avatar={AvatarPersona}
+                                    review="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure corrupti nemo dolore odio saepe ex? Cum nisi dolores, neque, reprehenderit suscipit nemo, debitis voluptas consequuntur harum possimus praesentium iure."
+                                />
+                            </Col>
+                            
                         </Row>
                     </Col>
                     <Col lg={4}/>
@@ -35,7 +81,7 @@ export default function ReviewCourses() {
      const {Meta }= Card;
 
      return (
-         <Card className="reviews-courses-__card">
+         <Card className="reviews-courses__card">
             <p>{review}</p>
             <Meta
                 avatar={<Avatar src={avatar}/>}
