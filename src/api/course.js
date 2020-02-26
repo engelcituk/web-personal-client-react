ba9674da316f8e3885e980abda5e283c49758666
cituk <programaciondyn3.mx@sandos.com>
2020-02-25 21:28:50 -0500

    Obteniendo los datos de los cursos de la API Udemy

--- src/api/course.js
+++ src/api/course.js
@@ -14,6 +14,22 @@ export function getCoursesApi() {
             return err;
         })
 }
+export function getCourseDataUdemyApi(id) {
+    const baseUrl = `https://www.udemy.com/api-2.0/courses/${id}`;
+    const courseParams = `?fields[course]=title,headline,url,price,image_480x270`;
+    const url= baseUrl+courseParams;
+
+    return fetch(url)
+        .then( async response => {
+            return {code: response.status, data: await response.json()};
+        })
+        .then(result => {
+            return result;
+        })
+        .catch(err => {
+            return err;
+        })
+}
 
 export function updateCourseApi(token, courseId, data) {
     const url = `${basePath}/${apiVersion}/course/${courseId}`;
--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './CoursesList.scss';
+import {getCourseDataUdemyApi} from '../../../../api/course';
+
 
 export default function CoursesList(props) {
     const {courses, setReloadCourses}=props;
-    console.log(props);
+
+    if(courses.length > 0){
+        courses.forEach(course => {
+            getCourseDataUdemyApi(course.idCourse).then( response => {
+                console.log(response)
+            }) 
+        });
+
+    }
     return (
         <div>
             <h1>cursos lista</h1>
