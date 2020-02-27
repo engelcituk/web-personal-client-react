5a2d39384aa46c0496a26f032286eb1e411405f3
cituk <programaciondyn3.mx@sandos.com>
2020-02-27 00:30:42 -0500

    Creando PresentationCourses

--- src/components/web/Courses/PresentationCourses/PresentationCourses.js
+++ src/components/web/Courses/PresentationCourses/PresentationCourses.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './PresentationCourses.scss';
+import LogoCursos from '../../../../assets/img/png/academy-logo.png';
 
 export default function PresentationCourses() {
     return (
-        <div>
-            loremjrgpirew
+        <div className="presentation-courses">
+            <img src={LogoCursos} alt="logo"/>
+            <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore aliquam magni repudiandae, ipsa eaque impedit amet ratione consequatur suscipit ab, hic saepe. Aliquid placeat, voluptatem maiores amet molestias harum inventore.
+            </p>
+            <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
         </div>
     )
 }
