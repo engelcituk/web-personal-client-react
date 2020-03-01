3df2b5bf5c04403cc36b0a10e53866df46fb1b37
cituk <programaciondyn3.mx@sandos.com>
2020-02-29 19:53:56 -0500

    Creando la sección del blog en el panel de administrador

--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Button, notification } from 'antd';
+import Modal from '../../../components/modal';
 import './Blog.scss';
 
 export default function Blog() {
+    const [isVisibleModal, setIsVisibleModal]= useState(false);
+    const [modalTitle, setModalTitle]= useState("");
+    const [modalContent, setModalContent]= useState(null);
+
     return (
-        <div>
-            <h1>Blog</h1>
+        <div className="blog">
+            <div className="blog__add-post">
+                <Button type="primary">
+                    Nuevo post
+                </Button>
+            </div>
+            <h1>post list</h1>
+            <h2>paginacion de nuestros posts</h2>
+            <Modal
+                title={modalTitle}
+                isVisible={isVisibleModal}
+                setIsVisible={setIsVisibleModal}
+                width="75%"
+            />
         </div>
     )
 }
